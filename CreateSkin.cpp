52fd28aaa6e39e05b3fe86f52b4f5fa90cfde5fd
Александр Кравец <kravets.aleksandr@hotmail.com>
2019-11-26 11:02:25 +0200

    commit

---
+++ CreateSkin.cpp
@@ -0,0 +1,32 @@
+#include "CreateSkin.hpp"
+#include <iostream>
+#include <fstream>
+#include <list>
+#include "Sphere.h"
+#include "PointCloud.h"
+#include "MoveSphere.h"
+
+
+void CreateSkin(const cadcam::mwTPoint3d<double> refPoint,
+	const unsigned long nx, const unsigned long ny,
+	const unsigned long nz, const double sphereRad,
+	mwDiscreteFunction& func, const double deltaT,
+	const double delta, const std::string& skinFileName)
+{
+
+	//Your source code here...
+
+	PointCloud _pointCloud(refPoint, nx, ny, nz, delta);
+
+	Sphere _sphere(point3d(9.5, 19.5, 0.), sphereRad);
+
+	MoveSphere _moveSphere(_sphere.GetCenter(), point3d(9.5, 19.5, 19.5), deltaT);
+
+	do
+	{
+		_pointCloud.RemovePoints(_moveSphere.GetCenter(), _sphere.GetRadius());
+	}
+	while ( _moveSphere() );
+
+	_pointCloud.Draw(skinFileName);
+}
---
+++ MoveSphere.cpp
@@ -0,0 +1,1 @@
+#include "MoveSphere.h"
